d45d585d5d6ded0e6d4f405651208ff148350c9a
Vanessa Laís Morais de Lima <vlml2001lima@gmail.com>
2022-09-08 17:01:56 -0300

    corrigindo nome do script

--- scripit.js
+++ scripit.js
@@ -1,3 +1,6 @@
-const inputTexto = document.querySelector(".texto-inicial")
-const mensagem = document.querySelector(".texto-resultante")
+const inputTexto = document.querySelector(".texto-inicial");
+const mensagem = document.querySelector(".texto-resultante");
 
+function botaoEencriptar() {
+	console.log("clicou");
+}
