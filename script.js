08cfa2fd04a009f737680032956cdeafa0d57e3a
Vanessa Laís Morais de Lima <vlml2001lima@gmail.com>
2022-09-08 20:57:08 -0300

    organizando arquivos

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const inputTexto = document.querySelector(".texto-inicial");
 const mensagem = document.querySelector(".texto-resultante");
 
 function botaoEncriptar(){
-	console.log("clic");// body...
+	alert("clicou");// body...
 }
