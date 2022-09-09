756944ac93b3e60a6f49f4550ff87802b397ad8f
Vanessa Laís Morais de Lima <vlml2001lima@gmail.com>
2022-09-08 21:45:33 -0300

    funções adicionadas

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const inputTexto = document.querySelector(".texto-inicial");
-const mensagem = document.querySelector(".texto-resultante");
+const inputTexto = document.querySelector("#texto-inicial");
+const mensagem = document.querySelector("#texto-resultante");
 
 function botaoEncriptar(){
-	alert("clicou");// body...
+	const textoEncriptado = encriptar(inputTexto.value);
+	mensagem.value = textoEncriptado;
 }
+
+function botaoDescriptar() {
+	const textoDescriptado = desencriptar(inputTexto.value);
+	mensagem.value = textoDescriptado;
+}
+
+function encriptar(stringEncriptada) {
+	let matrizCodigo = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']];
+	stringEncriptada = stringEncriptada.toLowerCase();
+
+	for (var i = 0; i < matrizCodigo.length; i++) {
+		if (stringEncriptada.includes(matrizCodigo[i][0])) {
+			stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+		}
+	}
+	return stringEncriptada;
+}
+
+function desencriptar(stringEncriptada) {
+	let matrizCodigo = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']]
+	stringEncriptada = stringEncriptada.toLowerCase();
+
+	for (var i = 0; i < matrizCodigo.length; i++) {
+		if (stringEncriptada.includes(matrizCodigo[i][1])) {
+			stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+		}
+	}
+	return stringEncriptada;
+}
+
+
+
+
